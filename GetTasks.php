71345f553d3241280172e1ac73fd0a8781262960
Christian <Christian@10.192.253.250>
2019-01-10 22:44:42 -0500

    Added for loop through days of the week

--- GetTasks.php
+++ GetTasks.php
@@ -8,10 +8,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die("Connection failed: " . $conn->connect_error);
     }
     
-    $task = json_decode($_POST["task"], false);
+    $stmt = $conn->prepare("SELECT taskID, title, description, time, color FROM tasks WHERE day=?");
+    $stmt->bind_param("s", $day);
     
-    $stmt = $conn->prepare("INSERT INTO tasks(`title`, `details`, `time`, `day`, `color`) VALUES (?, ?, ?, ?, ?)");   
-    $stmt->bind_param("sssss", $task->title, $task->details, $task->time, $task->day, $task->color);
+    $tasks = array();
+    
+    $days = array("mo", "tu", "we", "th", "fr", "sa", "su");
+    foreach ($days as $day) {
+        $stmt->execute();
+        $stmt->bind_result($taskID, $title, $description, $time, $color);
+    }
+    
+    //     $task = json_decode($_POST["task"], false);
+    
+    //     $stmt = $conn->prepare("INSERT INTO tasks(`title`, `details`, `time`, `day`, `color`) VALUES (?, ?, ?, ?, ?)");
+    //     $stmt->bind_param("sssss", $task->title, $task->details, $task->time, $task->day, $task->color);
     
     $stmt->execute();
     
